a613e14891e46a0f957e81931d032788c588ca5f
Nikhil Balyan <nbalyan10@gmail.com>
2019-07-18 12:59:43 +0530

    initialize repository

---
+++ src/main/java/com/springboot/efficient/developement/springboot/efficient/developement/SpringbootEfficientDevelopementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.efficient.developement.springboot.efficient.developement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEfficientDevelopementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEfficientDevelopementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/efficient/developement/springboot/efficient/developement/SpringbootEfficientDevelopementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.efficient.developement.springboot.efficient.developement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootEfficientDevelopementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
